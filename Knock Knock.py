bc5dbee0d7424738b95f34bb286c069532b09bea
Radi0Activ3-1 <drakepd100@gmail.com>
2025-01-27 20:29:06 -0600

    Add files via upload

---
+++ Knock Knock.py
@@ -0,0 +1,10 @@
+#Knock-Knock Joke Program
+print("Knock, Knock.")
+print("Who's There?")
+print("Gwen")
+print("Gwen Who?")
+print("Gwen will these jokes ever end?!")
+
+print()
+# As one line of code using escape characters
+print("Knock, knock.\nWho's There?\nGwen\nGwen Who?\nGwen will these jokes end?!")
